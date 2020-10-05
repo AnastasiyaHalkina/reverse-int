848a4afe470e46c09e719627763ce7407b00efbd
Anastasiya <schwarzer300708held@gmail.com>
2020-10-05 18:56:57 +0300

    feat: add Reverse int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+    let resultString = String(n).split('').reverse().join('');
+    return (!resultString.endsWith('-')) ? Number(resultString) : Number(resultString.slice(0, -1)) ;
 }
